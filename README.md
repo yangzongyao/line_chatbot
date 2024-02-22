0a36e5e1e34fbc9aa61471ded5a8484ef08c5e56
yangzongyao <andy831201@gmail.com>
2024-02-23 00:40:09 +0800

    ver 1

---
+++ README.md
@@ -0,0 +1,3 @@
+##
+啟動ngrok
+複製網址進入line後台的workhook
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # %%
+import requests
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -7,8 +8,8 @@ import os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+line_bot_api = LineBotApi('LrMgKNTRX8uzQkt1r1npaphUVeaWjA519o4CGQ1WcQXW+67qKVntkLpiZ3CTSrClgd81vxQ35P9+0pjIhwlB0saVNpQWcSZpsSWXEVB/QDENpDKuOm/DT5HBW/JzEMxSLzCQqDXKWMpxuyLl9Dlx3QdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('fba10ff5fcef3748c499bedeb3529f21')
 
 
 @app.route("/callback", methods=['POST'])
@@ -24,8 +25,18 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+
+    data = {
+        "model":"llama2", 
+        "prompt": event.message.text,
+        "stream": False
+    }
+    response = requests.post('http://localhost:11434/api/generate', json=data).json()
+    message = TextSendMessage(text=response["response"])
     line_bot_api.reply_message(event.reply_token, message)
+    # stricker_message = StickerSendMessage(sticker_id=446, package_id=1988)
+    # line_bot_api.reply_message(event.reply_token, stricker_message)
+
 
 import os
 if __name__ == "__main__":
